8d27ef46ba55ae3e3c8567487a2c2b0bec908023
Ståle <stale.jacobsen@student.uib.no>
2021-10-23 20:00:42 +0200

    Refactor Items

--- src/main/kotlin/com/example/models/Item.kt
+++ src/main/kotlin/com/example/models/Item.kt
@@ -1,20 +1,22 @@
 package com.example.models
 
-import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.Table
 
-object Items : Table() {
-    val id: Column<Int> = integer("id").autoIncrement()
-    val name: Column<String> = varchar("name", 255)
-
+object Items : IntIdTable() {
+    val name = varchar("name", 255)
     override val primaryKey = PrimaryKey(id, name = "PK_Item_ID")
 
     fun toItem(row: ResultRow): Item =
-        Item(
-            id = row[id],
-            name = row[name]
-        )
+        Item()
+
 }
 
-data class Item(val id: Int? = null, val name: String)
+class Item(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Item>(Items)
+
+    var name by Items.name
+}
