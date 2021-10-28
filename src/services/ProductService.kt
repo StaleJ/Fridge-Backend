64edf4f4e5ff1e810e630a35b581253a8e487909
Ståle <stale.jacobsen@student.uib.no>
2021-10-28 16:18:53 +0200

    Create ProductService.kt

---
+++ src/services/ProductService.kt
@@ -0,0 +1,23 @@
+package at.stefangaller.services
+
+import at.stefangaller.data.Product
+import at.stefangaller.data.ProductEntity
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class ProductService {
+
+    fun getAllProduct(): Iterable<Product> = transaction {
+        ProductEntity.all().map(ProductEntity::toProduct)
+    }
+
+    fun addProduct(product: Product) = transaction {
+        ProductEntity.new {
+            this.productName = product.productName
+            this.expiredDate = product.expiredDate
+        }
+    }
+
+    fun deleteProduct(productId: Int) = transaction {
+        ProductEntity[productId].delete()
+    }
+}
