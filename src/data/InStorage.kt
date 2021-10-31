3d7533b3c7cd5b604148f8d17d368ffb0abecb95
Ståle <stale.jacobsen@student.uib.no>
2021-10-31 17:41:23 +0100

    Update InStorage.kt

--- src/data/InStorage.kt
+++ src/data/InStorage.kt
@@ -6,26 +6,26 @@ import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object InStorages : IntIdTable() {
-    val storageId = reference("storageId", Storages)
-    val productId = reference("productId", Products)
+    val storage = reference("storageId", Storages)
+    val product = reference("productId", Products)
     val quantity = integer("quantity")
 }
 
 class InStorageEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<InStorageEntity>(InStorages)
 
-    var storageId by StorageEntity referencedOn InStorages.storageId
-    var productId by ProductEntity referencedOn InStorages.productId
+    var storage by StorageEntity referencedOn InStorages.storage
+    var product by ProductEntity referencedOn InStorages.product
     var quantity by InStorages.quantity
 
-    override fun toString(): String = "InStorage(${storageId.storageName}, ${productId.productName}, $quantity)"
+    override fun toString(): String = "InStorage(${storage.storageName}, ${product.productName}, $quantity)"
 
-    fun toInStorage() = InStorage(id.value, storageId.id.value, productId.id.value, quantity)
+    fun toInStorage() = InStorage(id.value, storage, product, quantity)
 }
 
 data class InStorage(
     val InStoragesId: Int,
-    val storageId: Int,
-    val productId: Int,
+    val storage: StorageEntity,
+    val product: ProductEntity,
     val quantity: Int
 )
