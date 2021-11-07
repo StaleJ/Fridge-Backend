b36ca2779b16a6881d6a0df58581c5a60e15ca96
Ståle <stale.jacobsen@student.uib.no>
2021-11-07 14:27:20 +0100

    refactor backend

--- src/data/InStorage.kt
+++ src/data/InStorage.kt
@@ -4,11 +4,15 @@ import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.`java-time`.datetime
+import java.time.LocalDateTime
+
 
 object InStorages : IntIdTable() {
     val storage = reference("storageId", Storages)
     val product = reference("productId", Products)
     val quantity = integer("quantity")
+    val expiredDate = datetime("expiredDate")
     override val primaryKey = PrimaryKey(storage, product, name = "PK_InStorage")
 }
 
@@ -18,14 +22,16 @@ class InStorageEntity(id: EntityID<Int>) : IntEntity(id) {
     var storage by StorageEntity referencedOn InStorages.storage
     var product by ProductEntity referencedOn InStorages.product
     var quantity by InStorages.quantity
+    var expiredDate by InStorages.expiredDate
 
-    override fun toString(): String = "InStorage(${storage.storageName}, ${product.productName}, $quantity)"
+    override fun toString(): String = "InStorage(${storage.storageName}, ${product.productName}, $quantity, $expiredDate)"
 
-    fun toInStorage() = InStorage(storage, product, quantity)
+    fun toInStorage() = InStorage(storage, product, quantity, expiredDate)
 }
 
 data class InStorage(
     val storage: StorageEntity,
     val product: ProductEntity,
-    val quantity: Int
+    val quantity: Int,
+    val expiredDate: LocalDateTime
 )
